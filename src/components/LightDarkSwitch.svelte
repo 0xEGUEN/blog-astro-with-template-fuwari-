d5f2d70b93af25facfb990d2f5494f6c16f804c0
0xEGUEN <adwarahmant@gmail.com>
2026-06-30 21:01:26 +0700

    fix type checks

--- src/components/LightDarkSwitch.svelte
+++ src/components/LightDarkSwitch.svelte
@@ -11,6 +11,9 @@ import {
 import { onMount } from "svelte";
 import type { LIGHT_DARK_MODE } from "@/types/config.ts";
 
+type Props = { "client:only"?: string };
+let _props: Props = $props();
+
 const seq: LIGHT_DARK_MODE[] = [LIGHT_MODE, DARK_MODE, AUTO_MODE];
 let mode: LIGHT_DARK_MODE = $state(AUTO_MODE);
 
